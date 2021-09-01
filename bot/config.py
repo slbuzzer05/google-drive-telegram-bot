0e05846503def963a7c0792d61094a7da2e5331b
slbuzzer05 <89919695+slbuzzer05@users.noreply.github.com>
2021-09-01 19:19:38 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
+    BOT_TOKEN = "1970014190:AAFY31b3F3AJXHlzh4aRbzxKfRhmxXThYwg"
+    APP_ID = "7556004"
+    API_HASH = "38334c77607adf01923c82a9f5149dcf"
+    DATABASE_URL = "postgres://qbffogvzqqdrec:0bb2cfe07dfc0ccd7a4ca22334dc6382d811524f6a155788114e918218e8b08f@ec2-54-145-188-92.compute-1.amazonaws.com:5432/d7s22o3euiqs4o"
+    SUDO_USERS = "567852320 779431236" # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/joinchat/1g9L7zXXG1AxNjI1"
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
     G_DRIVE_CLIENT_SECRET = ""
